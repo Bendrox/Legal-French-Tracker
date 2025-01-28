3fe1df38b7dbdfb255af23a7a63d6e4c898bb15f
Ouss Bendri <oussamabendrisspro@gmail.com>
2025-01-28 18:59:40 +0100

    Wrap up function

--- master_script.py
+++
@@ -1,9 +0,0 @@
-import main1
-import main2
-import main3
-
-if __name__ == "__main__":
-    # Appeler successivement les fonctions principales de chaque fichier
-    main1.main()
-    main2.main()
-    main3.main()
