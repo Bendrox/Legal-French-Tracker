426e60072becab822cdb077e699e5cf18bf2f925
Ouss Bendri <oussamabendrisspro@gmail.com>
2025-02-02 21:45:46 +0100

    Improving global code for handling cred

--- credentials.py
+++ credentials.py
@@ -8,7 +8,12 @@ LOGIN_EURLEX= os.getenv("LOGIN_EURLEX")
 MOT_DE_PASSE_EURLEX= os.getenv("MOT_DE_PASSE_EURLEX")
 AZURE_OPENAI_KEY= os.getenv("AZURE_OPENAI_KEY")
 api_key_firecraw = os.getenv("api_key_firecraw")
+
+### API legifrance
 client_id_pro = os.getenv("client_id_pro")
 client_secret_pro = os.getenv("client_secret_pro")
+client_id = os.getenv("client_id") 
+client_secret = os.getenv("client_secret")
+
 Azure_OpenAI_OB_Key = os.getenv("Azure_OpenAI_OB_Key")
 Azure_OpenAI_OB_Endpoint = os.getenv("Azure_OpenAI_OB_Endpoint")
--- modules_LLM/LLM_Analytic_changes.py
+++ modules_LLM/LLM_Analytic_changes.py
@@ -8,7 +8,6 @@ from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 
 from dotenv import load_dotenv
-
 load_dotenv()
 
 
